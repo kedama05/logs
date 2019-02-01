422b67c73df6d225f53e7f40a897071a09554381
kedama05 <karashi002@gmail.com>
2019-02-01 10:18:52 +0000

    mod

--- step/no001_github.sh
+++ step/no001_github.sh
@@ -1,4 +1,21 @@
+#!/bin/bash
+echo -n Input Github name:
+read name
+echo -n Input Github email:
+read email
+
 mkdir -p warehouse/git
 cd warehouse/git/
+# clone logs
 git clone https://github.com/kedama05/logs.git
+cd logs
+git config user.name $name
+git config user.email $email
+cd ..
+
+# clone init
 git clone https://github.com/kedama05/init.git
+cd init
+git config user.name $name
+git config user.email $email
+cd ..
